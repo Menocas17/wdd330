082ba5b129d04114b2dbfd104ac140414e96708b
Menocas17 <rodolfomenocal7@gmail.com>
2025-02-20 18:01:36 -0600

    adding the ham buttom functionalitiy

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -40,6 +40,16 @@ export async function loadHeaderFooter () {
 
     renderTemplate(header, headerElement);
     renderTemplate(footer, footerElement);
+
+
+const hamButton = document.querySelector('#ham-menu');
+const navMenu = document.querySelector('.menu');
+
+
+hamButton.addEventListener('click', () => {
+    hamButton.classList.toggle('open');
+    navMenu.classList.toggle('open');
+})
 }
 
 // function to add the event listener to the card elements
